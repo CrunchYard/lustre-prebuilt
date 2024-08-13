d42443453c1df58150728800814a405c23d19c3c
Ken Nixon <nixon.kj@gmail.com>
2024-08-13 11:53:49 +0200

    Create check-single.sh

---
+++ scripts/check-single.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    exit 1
+fi
+
+export FILE=$1
+
+rbh-report -e $FILE | gawk -F'\t' 'START{print("filename,stripe_cnt,stripe_size");}{split($1,field,","); split($2,value,","); gsub(/ /,"",value[1]); gsub(/ /,"",value[2]); stripe_cnt=value[1]; stripe_size=value[2]; if(field[1]=="stripe_cnt"&&stripe_cnt<2) printf("%s,%s,%s", ENVIRON["FILE"], stripe_cnt, stripe_size);}'
