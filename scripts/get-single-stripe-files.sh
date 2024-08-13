d391b3a5518c6b54ef796b69be2f8434359a6007
Ken Nixon <nixon.kj@gmail.com>
2024-08-13 12:08:16 +0200

    Update get-single-stripe-files.sh

--- scripts/get-single-stripe-files.sh
+++ scripts/get-single-stripe-files.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rbh-find "$(pwd -P)" -size +10M -exec "./check-single.sh {}" \; > single-stripe-files.csv 2> /dev/null
+echo "filename,stripe_cnt,stripe_size" > single-stripe-files.csv
+rbh-find "$(pwd -P)" -size +10M -exec "./check-single.sh {}" \; >> single-stripe-files.csv 2> /dev/null
