c84c66c83840680987c683244de4e4c24021ae7e
Ken Nixon <nixon.kj@gmail.com>
2024-08-13 12:00:34 +0200

    Update check-single.sh

--- scripts/check-single.sh
+++ scripts/check-single.sh
@@ -7,4 +7,4 @@ fi
 
 export FILE=$1
 
-rbh-report -e $FILE | gawk -F'\t' 'START{print("filename,stripe_cnt,stripe_size");}{split($1,field,","); split($2,value,","); gsub(/ /,"",value[1]); gsub(/ /,"",value[2]); stripe_cnt=value[1]; stripe_size=value[2]; if(field[1]=="stripe_cnt"&&stripe_cnt<2) printf("%s,%s,%s", ENVIRON["FILE"], stripe_cnt, stripe_size);}'
+rbh-report -e $FILE | gawk -F'\t' 'BEGIN{print("filename,stripe_cnt,stripe_size");}{split($1,field,","); split($2,value,","); gsub(/ /,"",value[1]); gsub(/ /,"",value[2]); stripe_cnt=value[1]; stripe_size=value[2]; if(field[1]=="stripe_cnt"&&stripe_cnt<2) printf("%s,%s,%s", ENVIRON["FILE"], stripe_cnt, stripe_size);}'
