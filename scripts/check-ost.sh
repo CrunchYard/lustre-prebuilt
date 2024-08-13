9eeab9da7271d5df973740e165cb5197437cca32
Ken Nixon <nixon.kj@gmail.com>
2024-08-13 12:01:08 +0200

    Update check-ost.sh

--- scripts/check-ost.sh
+++ scripts/check-ost.sh
@@ -8,4 +8,4 @@ fi
 export FILE=$1
 export OST=$2
 
-rbh-report -e $FILE | gawk -F'\t' 'START{print("filename");}{ost=sprintf("ost#%s:",ENVIRON["OST"]); if(substr($1,0,7)=="stripes"&&index($2,ost)) printf("%s ", ENVIRON["FILE"]);}'
+rbh-report -e $FILE | gawk -F'\t' 'BEGIN{print("filename");}{ost=sprintf("ost#%s:",ENVIRON["OST"]); if(substr($1,0,7)=="stripes"&&index($2,ost)) printf("%s ", ENVIRON["FILE"]);}'
