3a71d89554305897df414dae32988073774c8622
Ken Nixon <nixon.kj@gmail.com>
2024-08-13 11:53:20 +0200

    Create check-ost.sh

---
+++ scripts/check-ost.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    exit 1
+fi
+
+export FILE=$1
+export OST=$2
+
+rbh-report -e $FILE | gawk -F'\t' 'START{print("filename");}{ost=sprintf("ost#%s:",ENVIRON["OST"]); if(substr($1,0,7)=="stripes"&&index($2,ost)) printf("%s ", ENVIRON["FILE"]);}'
