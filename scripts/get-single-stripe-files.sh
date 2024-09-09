cfc50ba825fc014b2d563dd99d965d2f186e8fd7
Ken Nixon <nixon.kj@gmail.com>
2024-09-09 20:28:50 +0200

    Delete scripts/get-single-stripe-files.sh

--- scripts/get-single-stripe-files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "filename,stripe_cnt,stripe_size" > single-stripe-files.csv
-rbh-find "$(pwd -P)" -size +10M -exec "./check-single.sh {}" \; >> single-stripe-files.csv 2> /dev/null
